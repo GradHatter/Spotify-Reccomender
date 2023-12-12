6b8d711d1d0808405b1c5a47611d36b56e79615d
David King <40282634+GradHatter@users.noreply.github.com>
2023-12-12 10:09:57 -0500

    Add files via upload

---
+++ spotify_example.py
@@ -0,0 +1,115 @@
+import requests
+import base64
+
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Replace with your own Client ID and Client Secret
+CLIENT_ID = '52f7cfd2ef2c41f89685df88e3772f55'
+CLIENT_SECRET = 'de8f11bd0e51450cbff4ea897009eb14'
+
+# Base64 encode the client ID and client secret
+client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+client_credentials_base64 = base64.b64encode(client_credentials.encode())
+
+# Request the access token
+token_url = 'https://accounts.spotify.com/api/token'
+headers = {
+    'Authorization': f'Basic {client_credentials_base64.decode()}'
+}
+data = {
+    'grant_type': 'client_credentials'
+}
+response = requests.post(token_url, data=data, headers=headers)
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+    print("Access token obtained successfully.")
+else:
+    print("Error obtaining access token.")
+    exit()
+    
+#------------------------------------------------------------------------------
+
+#Get song data from a playlist and turn it into a useable dataframe
+
+def playlist_song_data(playlist_id, access_token):
+    # Set up Spotipy with the access token
+    sp = spotipy.Spotify(auth=access_token)
+
+    # Get the tracks from the playlist
+    playlist_tracks = sp.playlist_tracks(playlist_id,
+                        fields='items(track(id, name, artists, album(id, name)))')
+
+    # Extract relevant information and store in a list of dictionaries
+    music_data = []
+    for track_info in playlist_tracks['items']:
+        track = track_info['track']
+        track_name = track['name']
+        artists = ', '.join([artist['name'] for artist in track['artists']])
+        album_name = track['album']['name']
+        album_id = track['album']['id']
+        track_id = track['id']
+
+        # Get audio features for the track
+        audio_features = sp.audio_features(track_id)[0] if track_id != 'Not available' else None
+
+        # Get release date of the album
+        try:
+            album_info = sp.album(album_id) if album_id != 'Not available' else None
+            release_date = album_info['release_date'] if album_info else None
+        except:
+            release_date = None
+
+        # Get popularity of the track
+        try:
+            track_info = sp.track(track_id) if track_id != 'Not available' else None
+            popularity = track_info['popularity'] if track_info else None
+        except:
+            popularity = None
+
+        # Add additional track information to the track data
+        track_data = {
+            'Track Name': track_name,
+            'Artists': artists,
+            'Album Name': album_name,
+            'Album ID': album_id,
+            'Track ID': track_id,
+            'Popularity': popularity,
+            'Release Date': release_date,
+            'Duration (ms)': audio_features['duration_ms'] if audio_features else None,
+            'Explicit': track_info.get('explicit', None),
+            'External URLs': track_info.get('external_urls', {}).get('spotify', None),
+            'Danceability': audio_features['danceability'] if audio_features else None,
+            'Energy': audio_features['energy'] if audio_features else None,
+            'Key': audio_features['key'] if audio_features else None,
+            'Loudness': audio_features['loudness'] if audio_features else None,
+            'Mode': audio_features['mode'] if audio_features else None,
+            'Speechiness': audio_features['speechiness'] if audio_features else None,
+            'Acousticness': audio_features['acousticness'] if audio_features else None,
+            'Instrumentalness': audio_features['instrumentalness'] if audio_features else None,
+            'Liveness': audio_features['liveness'] if audio_features else None,
+            'Valence': audio_features['valence'] if audio_features else None,
+            'Tempo': audio_features['tempo'] if audio_features else None,
+        }
+
+        music_data.append(track_data)
+
+    # Create a pandas DataFrame from the list of dictionaries
+    df = pd.DataFrame(music_data)
+
+    return df
+
+#------------------------------------------------------------------------------
+
+playlist_id = '1bNZQV7Kjf5P8md4C3Xzey?si=a0b4facc21ac44ce'
+
+# Call the function to get the music data from the playlist and store it in a DataFrame
+music_df = playlist_song_data(playlist_id, access_token)
+
+#print(music_df.head())
+
+playlist_name = ""
+
+df.to_csv(f"{playlist_name}.csv")
